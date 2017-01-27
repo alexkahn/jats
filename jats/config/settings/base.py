ef3068db2fea5ca376253897a15b6912d4547cee
Alex Kahn <alex.kahn@outlook.com>
2017-01-27 15:15:49 -0600

    change static root for deployment

--- jats/config/settings/base.py
+++ jats/config/settings/base.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(APP_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(APP_DIR, 'static'),
